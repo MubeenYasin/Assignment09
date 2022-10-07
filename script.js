f49901496cba2274ae782ddb5e1c2f216001acbd
Mubeen Yasin <65570764+MubeenYasin@users.noreply.github.com>
2022-10-07 10:49:39 +0500

    Problem is starting to solve

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("IN THE NAME OF ALLAH")
+
+function calculateNotes() {
+    var notes = document.getElementById('notes').value;
+
+    if (notes === '20') {
+        console.log("note 20")
+    }
+}
